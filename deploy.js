1a16e1e0942e77089f675d41cb78604341c8f7c8
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2015-03-17 23:00:08 -0230

    Refactor deploy logic

--- deploy.js
+++ deploy.js
@@ -1,73 +1,42 @@
-var AWS = require('aws-sdk');
+var Promise = require("bluebird");
+var AWS = require("aws-sdk");
+
 var s3 = new AWS.S3();
 var cloudFront = new AWS.CloudFront();
+var domain = "whymarrh.com";
 
-var getDistributionId = function getDistributionId(domain, callback) {
-	cloudFront.listDistributions(function cb(err, data) {
-		if (err) {
-			return callback(err);
-		}
-		var items = data.Items || [];
-		var ids = data.Items.filter(function filterDistributionByAlias(item) {
-			return item.Aliases.Quantity === 1 && item.Aliases.Items[0] === domain;
-		}).map(function mapDistributionsToIds(item) {
-			return item.Id;
-		});
-		callback(undefined, ids.pop());
-	});
-};
-
-var getCurrentObjects = function getCurrentObjects(bucket, callback) {
-	s3.listObjects({
-		Bucket: bucket
-	}, function cb(err, data) {
-		if (err) {
-			return callback(err);
-		}
-		var keys = data.Contents.map(function (item) {
-			return item.Key;
-		});
-		callback(undefined, keys);
-	});
-};
-
-var invalidateObjects = function invalidateObjects(objects, distributionId, callback) {
-	cloudFront.createInvalidation({
-		DistributionId: distributionId,
-		InvalidationBatch: {
-			CallerReference: (new Date() * 1).toString(),
-			Paths: {
-				Quantity: objects.length,
-				Items: objects.map(function prefix(object) {
-					return '/' + object;
-				})
+var createCfInvalidation = function createCfInvalidation(paths) {
+	return Promise.promisify(cloudFront.listDistributions, cloudFront)({
+		// Defaults
+	})
+	.then(function (list) {
+		return (list.Items || []).filter(_ => _.Aliases.Items[0] === domain).map(_ => _.Id).pop();
+	})
+	.then(function (id) {
+		return Promise.promisify(cloudFront.createInvalidation, cloudFront)({
+			DistributionId: id,
+			InvalidationBatch: {
+				CallerReference: (new Date() * 1).toString(),
+				Paths: {
+					Quantity: paths.length,
+					Items: paths.map(o => '/' + o)
+				}
 			}
-		}
-	}, function cb(err, data) {
-		if (err) {
-			return callback(err);
-		}
-		callback(undefined, data);
+		});
 	});
 };
 
-var processError = function processError(err) {
-	if (!err) {
-		return;
-	}
-	console.error(err);
+Promise.promisify(s3.listObjects, s3)({
+	Bucket: domain
+})
+.then(function (data) {
+	return data.Contents.map(_ => _.Key);
+})
+.then(function (keys) {
+	return createCfInvalidation(keys);
+})
+.tap(console.log)
+.caught(function (err) {
 	console.error(err.stack);
 	process.exit(1);
-};
-
-var domain = "whymarrh.com";
-getCurrentObjects(domain, function processCurrentObjects(err, keys) {
-	processError(err);
-	getDistributionId(domain, function invalidateCurrentObjects(err, distributionId) {
-		processError(err);
-		invalidateObjects(keys, distributionId, function logResult(err, result) {
-			processError(err);
-			console.log(result);
-		});
-	});
 });

b0a6dc8bc7452af21332c7cba90791a122533fdf
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2015-08-30 22:57:26 -0230

    Rewrite deploy script

--- deploy.js
+++ deploy.js
@@ -5,30 +5,19 @@ var s3 = new AWS.S3();
 var cloudFront = new AWS.CloudFront();
 var domain = "whymarrh.com";
 
-var createCfInvalidation = function createCfInvalidation(paths) {
-	return Promise.promisify(cloudFront.listDistributions, cloudFront)({
-		// Defaults
-	})
-	.then(function (list) {
-		var id = (list.Items || []).filter(_ => _.Aliases.Items[0] === domain).map(_ => _.Id).pop();
-		return Promise.promisify(cloudFront.createInvalidation, cloudFront)({
-			DistributionId: id,
-			InvalidationBatch: {
-				CallerReference: (new Date() * 1).toString(),
-				Paths: {
-					Quantity: paths.length,
-					Items: paths.map(o => "/" + o)
-				}
+var createCfInvalidation = function createCfInvalidation(distributionId, ...items) {
+	return Promise.promisify(cloudFront.createInvalidation, cloudFront)({
+		DistributionId: distributionId,
+		InvalidationBatch: {
+			CallerReference: (new Date() * 1).toString(),
+			Paths: {
+				Quantity: items.length,
+				Items: items
 			}
-		});
+		}
 	});
 };
 
-Promise.promisify(s3.listObjects, s3)({
-	Bucket: domain
-})
-.then(data => createCfInvalidation(data.Contents.map(_ => _.Key)))
-.caught(function (err) {
-	console.error(err.stack);
-	process.exit(1);
-});
+Promise.promisify(cloudFront.listDistributions, cloudFront)({ /* Defaults */ })
+.then(distributions => distributions.Items.filter(_ => _.Aliases.Items[0] === domain)[0].Id)
+.then(id => createCfInvalidation(id, "/*"));

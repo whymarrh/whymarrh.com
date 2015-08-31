d14eddab42b069bb1aef2e1c2bbffdcea428297e
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2015-08-30 23:05:58 -0230

    Rewrite template plugin to use talk template

--- plugins/template.js
+++ plugins/template.js
@@ -1,40 +1,35 @@
-     async = require('async')
-handlebars = require('consolidate').handlebars
-         _ = require('lodash')
+var async = require('async');
+var handlebars = require('consolidate').handlebars;
+var _ = require('lodash');
 
-isHtml = RegExp.prototype.test.bind(/\.html$/)
+var isHtml = RegExp.prototype.test.bind(/\.html$/);
 
 module.exports = function template(opts) {
 	return function (files, metalsmith, done) {
-		keys = Object.keys(files)
-		async.each(
-			keys,
-			function (file, complete) {
-				data = files[file]
-				if (
-					   !isHtml(file)
-					|| !data.wasMarkdown
-				) {
-					return complete()
-				}
-				options = _.assign({}, metalsmith.metadata(), data)
-				handlebars(
-					// Template name
-					metalsmith.path('templates', "article.html"),
-					// Options
-					options,
-					// Callback fn
-					(function (data, file, err, str) {
-						if (err) {
-							return complete(err)
-						}
-						data.contents = new Buffer(str)
-						files[file] = data
-						complete()
-					}).bind(this, data, file)
-				)
-			},
-			done
-		)
-	}
-}
+		var teplateFile = function teplateFile(file, done) {
+			var data = files[file];
+
+			if (!isHtml(file) || !data.wasMarkdown) {
+				return done();
+			}
+
+			var options = _.assign({}, metalsmith.metadata(), data);
+			handlebars(
+				// Template name
+				metalsmith.path('templates', ~file.indexOf('talks') ? 'slides.html' : 'article.html'),
+				// Options
+				options,
+				// Callback
+				(function (data, file, err, str) {
+					if (err) {
+						return done(err);
+					}
+					data.contents = new Buffer(str);
+					files[file] = data;
+					done();
+				}).bind(this, data, file)
+			);
+		};
+		async.each(Object.keys(files), teplateFile, done);
+	};
+};
